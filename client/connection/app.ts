17fd16605971e230fe582001dda8c13e0ec66a13
shy-blue-sky <hyeyoonsung@gmail.com>
2026-01-12 16:44:04 -0500

    front end update with database

--- client/connection/app.ts
+++ client/connection/app.ts
@@ -3,10 +3,10 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import { intakeNaturalLanguageQuery } from './controller/naturalLanguageController.js';
-import { openAIQuery } from './controller/openAIController.js';
+import { convertToSQL } from './controller/openAIController.js';
 import { queryDatabase } from './controller/databaseController.js';
 
-import { ServerError } from './types';
+import { ServerError } from './types.ts';
 
 const app = express();
 
@@ -16,12 +16,15 @@ app.use(express.json());
 app.post(
     '/api',
     intakeNaturalLanguageQuery,
-    openAIQuery,
+    convertToSQL,
     queryDatabase,
     (_req, res) => {
       res.status(200).json({
         databaseQuery: res.locals.databaseQuery,
         databaseQueryResult: res.locals.databaseQueryResult,
       });
+    }
 
 )
+
+export default app;
